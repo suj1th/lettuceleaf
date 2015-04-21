6e6adb8edc8c4785214ed3a7e965d44e07d919bc
suj1th <sujith.nair@weareholidays.com>
2015-04-22 02:21:49 +0530

    Defined private no-arg constructor in ObjectMapperFactory

--- src/main/java/com/suj1th/lettuceleaf/ObjectMapperFactory.java
+++ src/main/java/com/suj1th/lettuceleaf/ObjectMapperFactory.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ObjectMapperFactory {
 	
+	/**
+	 * private constructor
+	 */
+	private ObjectMapperFactory(){}
+	
 	private static ObjectMapper objectMapper = new ObjectMapper();
 
 	public static ObjectMapper getObjectMapper() {
